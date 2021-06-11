0f62aa5ed27f7cc1b9d988b0bab97829b6dc7716
zulfikar-dityaa <zulfikarditya.work@gmail.com>
2021-06-11 14:04:52 +0700

    konsep dashboard

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -28,6 +28,7 @@ use loutrux\argon\widgets\Alert;
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://zulfikar-dityaa.github.io/base-css/style.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+        <script src="https://pagecdn.io/lib/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"  ></script>
         <style>
             body {
                 font-family: 'PT Sans', sans-serif;
@@ -178,7 +179,9 @@ use loutrux\argon\widgets\Alert;
         <?php $this->endBody() ?>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-        
+        <script>
+            feather.replace()
+        </script>
     </body>
 </html>
 <?php $this->endPage() ?>
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -2,8 +2,199 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+
 $this->title = 'Dashboard';
 ?>
 
+<?php 
+if (Yii::$app->user->identity->role_id == 1) { ?>
 <div class="bg-white shadow rounded-sm-2 p-md-1">
+    <div class="row justify-content-center">
+        <div class="col-md-4 p-3">
+            <div class="card shadow rounded-sm-2 p-3 border border-cyan">
+                <div class="card-body">
+                    <div class="text-center">
+                        <i data-feather="users" class="soft-bg-rose rounded-circle shadow text-rose p-2 border border-rose" style="width: 90px; height: 90px"></i>
+                        <div class="my-4">
+                            <h3 class="mb-0">Users</h3>
+                            <span class="mb-0 text-gray">50</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer bg-white">
+                    <?= Html::a('Create', ['user/create'], $options = ['class' => 'btn btn-block bg-rose text-white border-white']) ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 p-3">
+            <div class="card shadow rounded-sm-2 p-3 border border-cyan">
+                <div class="card-body">
+                    <div class="text-center">
+                        <i data-feather="users" class="soft-bg-rose rounded-circle shadow text-rose p-2 border border-rose" style="width: 90px; height: 90px"></i>
+                        <div class="my-4">
+                            <h3 class="mb-0">Users</h3>
+                            <span class="mb-0 text-gray">50</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer bg-white">
+                    <?= Html::a('Create', ['user/create'], $options = ['class' => 'btn btn-block bg-rose text-white border-white']) ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 p-3">
+            <div class="card shadow rounded-sm-2 p-3 border border-cyan">
+                <div class="card-body">
+                    <div class="text-center">
+                        <i data-feather="users" class="soft-bg-rose rounded-circle shadow text-rose p-2 border border-rose" style="width: 90px; height: 90px"></i>
+                        <div class="my-4">
+                            <h3 class="mb-0">Users</h3>
+                            <span class="mb-0 text-gray">50</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer bg-white">
+                    <?= Html::a('Create', ['user/create'], $options = ['class' => 'btn btn-block bg-rose text-white border-white']) ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<div class="bg-white shadow rounded-sm-2 p-md-1">
+    <div class="row justify-content">
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+        <div class="col-md-3 p-2">
+            <div class="card soft-bg-cyan shadow rounded-sm-2 p-3 border border-cyan">
+                <h6 class="fw-light mb-0 text-cyan" style="letter-spacing: 0px;">Nama Poli</h6>
+                <div class="my-3">
+                    <h3 class="mb-0">70</h3>
+                    <small class="text-gray mb-0">Antrian Sekarang</small>
+                </div>
+                <a href="#" class="btn btn-block bg-light-blue rounded-sm-2 border-white text-white">Next</a>
+            </div>
+        </div>
+    </div>
+</div>
+    <?php } ?>
+    <?php 
+    if (Yii::$app->user->identity->role_id == 2) { ?>
+
+    <?php } ?>
+    <?php 
+    if (Yii::$app->user->identity->role_id == 3) { ?>
+
+    <?php } ?>
+    <?php 
+    if (Yii::$app->user->identity->role_id == 4) { ?>
+
+    <?php } ?>
